4da96007387145807461a7cdf522a2785a9e5891
Mike Berkman <mike@berkman.co>
2019-10-11 14:28:55 -0400

    updates

--- scripts/upgrade-home-assistant.sh
+++ scripts/upgrade-home-assistant.sh
@@ -1,1 +1,4 @@
-docker pull homeassistant/raspberrypi3-homeassistant:latest
+docker stop home-assistant
+docker rm home-assistant
+docker pull homeassistant/raspberrypi4-homeassistant:latest
+docker run -d --name="home-assistant" -v /home/mberkman/home-assistant-config/:/config -e "TZ=America/New_York" --net=host homeassistant/raspberrypi4-homeassistan
